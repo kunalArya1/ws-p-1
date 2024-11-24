50d01f7ffa8838ffdb3910e6583793e4982dce48
kunalArya1 <kunalkrraj220@gmail.com>
2024-11-24 14:41:35 +0530

    Ping Pong App done

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello World!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+//Event handler for connection event
+wss.on("connection", function (socket) {
+    // console.log("Client connected");
+    //   setInterval(() => {
+    //     socket.send("Sol Proce " + Math.random());
+    //   }, 500);
+    socket.on("message", (e) => {
+        if (e.toString().includes("ping")) {
+            socket.send("pong");
+        }
+    });
+    socket.send("Hello! Welcome to the server");
+});
+// try frame
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//Event handler for connection event
+wss.on("connection", function (socket) {
+  // console.log("Client connected");
+//   setInterval(() => {
+//     socket.send("Sol Proce " + Math.random());
+//   }, 500);
+
+  socket.on("message", (e) => {
+    if (e.toString().includes("ping")) {
+      socket.send("pong");
+    }
+  });
+  socket.send("Hello! Welcome to the server");
+});
+
+// try frame
